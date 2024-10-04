935c5fff3e59c6375c74b3412fb6760c6e166f22
Pabbraga <pabbragadev@gmail.com>
2024-10-04 20:10:27 -0300

    feat: add delete exception handling

---
+++ src/main/java/com/pabbraga/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.pabbraga.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
