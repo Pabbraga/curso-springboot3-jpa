b466926565cfaaf90a9d89db79cd8b9789b0b762
Pabbraga <pabbragadev@gmail.com>
2024-10-04 19:58:17 -0300

    feat: add findbyid exception handling

---
+++ src/main/java/com/pabbraga/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.pabbraga.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
